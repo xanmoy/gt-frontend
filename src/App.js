bd2853c7330cdb998a87d90dee58525c3bf7c0d5
Tanmoy Ganguly <contact@xanmoy.me>
2024-04-30 21:30:29 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import './App.css';
-import Navbar from './components/navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Shop from "./pages/Shop";
+import ShopCategory from "./pages/ShopCategory";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import LoginSignup from "./pages/LoginSignup";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+      </Router>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/ethnic" element={<ShopCategory category="ethnic" />} />
+        <Route path="/western" element={<ShopCategory category="western" />} />
+        <Route path="/perfume" element={<ShopCategory category="perfume" />} />
+        <Route path="/product" element={<Product />}>
+          <Route path=":productId" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import './Navbar.css'
 import Logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-          <div className="nav-logo">
-              <img src={Logo} alt="Logo" />
-              <p>Glamour Threads</p>
-          </div>
-          <ul className='nav-menu'>
-              <li>Shop<hr/></li>
-              <li>Ethnic</li>
-              <li>Western</li>
-              <li>Perfume</li>
-          </ul>
-          <div className="nav-login-cart">
-              <button>Login</button>
-              <img src={cart_icon} alt="cart" />
-          </div>
-    </div>
-  )
+    const [menu, setMenu] = useState("shop");
+    return (
+
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={Logo} alt="Logo" />
+                <p>Glamour Threads</p>
+            </div>
+            <ul className='nav-menu'>
+                {/* <li onClick={() => { setMenu("shop") }}>Shop{ menu ==="shop"?}</li> */}
+                <li onClick={() => { setMenu("shop") }} style={{ color: menu === "shop" ? "#ff4141" : "#626262" }}><Link to='/'>Shop</Link></li>
+
+                <li onClick={() => { setMenu("ethnic") }} style={{ color: menu === "ethnic" ? "#ff4141" : "#626262" }}><Link to='/ethnic'>Ethnic</Link></li>
+                <li onClick={() => { setMenu("western") }} style={{ color: menu === "western" ? "#ff4141" : "#626262" }}><Link to='/western'>Western</Link></li>
+                <li onClick={() => { setMenu("perfume") }} style={{ color: menu === "perfume" ? "#ff4141" : "#626262" }}><Link to="/perfume">Perfume</Link>Perfume</li>
+            </ul>
+            <div className="nav-login-cart">
+                <Link to='/login'><button>Login</button></Link>
+                <link to='/cart'><img src={cart_icon} alt="cart" /></link>
+                <div className="nav-cart-count">0</div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
